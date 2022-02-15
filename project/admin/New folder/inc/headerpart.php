e21eab031b2cdfbc59e6d6513ce0dfa1de33330d
thedhaivatbhatt <95678661+thedhaivatbhatt@users.noreply.github.com>
2022-02-15 17:07:17 +0530

    changes

--- project/admin/New folder/inc/headerpart.php
+++ project/admin/New folder/inc/headerpart.php
@@ -10,31 +10,31 @@
     <meta content="" name="keywords" />
 
     <!-- Favicons -->
-    <link href="theme/assets/img/favicon.png" rel="icon" />
-    <link href="theme/assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
+    <link href="theme/niceadmin/assets/img/favicon.png" rel="icon" />
+    <link href="theme/niceadmin/assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
 
     <link
       href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
       rel="stylesheet"
     />
     <link
-      href="theme/assets/vendor/bootstrap/css/bootstrap.min.css"
+      href="theme/niceadmin/assets/vendor/bootstrap/css/bootstrap.min.css"
       rel="stylesheet"
     />
     <link
-      href="theme/assets/vendor/bootstrap-icons/bootstrap-icons.css"
+      href="theme/niceadmin/assets/vendor/bootstrap-icons/bootstrap-icons.css"
       rel="stylesheet"
     />
     <link
-      href="theme/assets/vendor/boxicons/css/boxicons.min.css"
+      href="theme/niceadmin/assets/vendor/boxicons/css/boxicons.min.css"
       rel="stylesheet"
     />
-    <link href="theme/assets/vendor/quill/quill.snow.css" rel="stylesheet" />
-    <link href="theme/assets/vendor/quill/quill.bubble.css" rel="stylesheet" />
-    <link href="theme/assets/vendor/remixicon/remixicon.css" rel="stylesheet" />
+    <link href="theme/niceadmin/assets/vendor/quill/quill.snow.css" rel="stylesheet" />
+    <link href="theme/niceadmin/assets/vendor/quill/quill.bubble.css" rel="stylesheet" />
+    <link href="theme/niceadmin/assets/vendor/remixicon/remixicon.css" rel="stylesheet" />
     <link
-      href="theme/assets/vendor/simple-datatables/style.css"
+      href="theme/niceadmin/assets/vendor/simple-datatables/style.css"
       rel="stylesheet"
     />
-    <link href="theme/assets/css/style.css" rel="stylesheet" />
+    <link href="theme/niceadmin/assets/css/style.css" rel="stylesheet" />
 <!-- end of first reusable part -->
--- project/admin/New folder/inc/sidebar.php
+++ project/admin/New folder/inc/sidebar.php
@@ -2,7 +2,7 @@
  <header id="header" class="header fixed-top d-flex align-items-center">
       <div class="d-flex align-items-center justify-content-between">
         <a href="index.html" class="logo d-flex align-items-center">
-          <img src="theme/assets/img/logo.png" alt="" />
+          <img src="theme/niceadmin/assets/img/logo.png" alt="" />
           <span class="d-none d-lg-block">Project Title</span>
         </a>
         <i class="bi bi-list toggle-sidebar-btn"></i>
