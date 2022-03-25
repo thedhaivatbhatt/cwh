5a1f5a18d1a29de39f81b0efd6291785e3f1142e
thedhaivatbhatt <95678661+thedhaivatbhatt@users.noreply.github.com>
2022-03-25 16:13:30 +0530

    changes

--- project/admin/advertiser.php
+++ project/admin/advertiser.php
@@ -32,49 +32,54 @@ require_once("inc/header.php");
 
             <!-- Blank Start -->
             <main id="main" class="main">
-        <div class="pagetitle">
-            <h5>Customer Management</h5>
-        </div>
-        <!-- End Page Title -->
-        <section class="section">
-
-            <!--end of row-->
-            <div class="row">
-                <div class="col-12">
+                <div class="pagetitle">
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title border-bottom">Existing Customer</h5>
-                            <table class="table datatable">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">Name</th>
-                                        <th scope="col">Contact</th>
-                                        <th scope="col">Mobile(s)</th>
-                                        <th scope="col">Last Payment Detail</th>
-                                        <th scope="col">Payment Due</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td><a href='advertiser_detail.php' title='click to view detail'>abc xyz</a></td>
-                                        <td>Mr xyz</td>
-                                        <td>1234567890 <br>
-                                            9876543210</td>
-                                        <td>01-feb-2022 <br> Rs 2500</td>
-                                        <td><a href='booked_advertise.php'>Rs. 5000</a></td>
-                                    </tr>
+                            <h5 class="card-title">Customer Management</h5>
 
-                                </tbody>
-                            </table>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    </main>
-    
+                    <!-- End Page Title -->
+                    <section class="section">
+
+                        <!--end of row-->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title border-bottom">Existing Customer</h5>
+                                        <table class="table datatable">
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col">#</th>
+                                                    <th scope="col">Name</th>
+                                                    <th scope="col">Contact</th>
+                                                    <th scope="col">Mobile(s)</th>
+                                                    <th scope="col">Last Payment Detail</th>
+                                                    <th scope="col">Payment Due</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <th scope="row">1</th>
+                                                    <td><a href='advertiser_detail.php' title='click to view detail'>abc xyz</a></td>
+                                                    <td>Mr xyz</td>
+                                                    <td>1234567890 <br>
+                                                        9876543210</td>
+                                                    <td>01-feb-2022 <br> Rs 2500</td>
+                                                    <td><a href='booked_advertise.php'>Rs. 5000</a></td>
+                                                </tr>
+
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+            </main>
+
             <!-- Blank End -->
 
 
