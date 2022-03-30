e424821b13eedbabedb5f2faec85a945c7388cb8
thedhaivatbhatt <95678661+thedhaivatbhatt@users.noreply.github.com>
2022-03-30 16:06:27 +0530

    changes

---
+++ project/admin/inc/menu.php
@@ -0,0 +1,29 @@
+<header>
+	<nav class="navbar navbar-expand-lg navigation" id="navbar">
+		<div class="container">
+		 	 <a class="navbar-brand" href="index.html">
+			  	<img src="theme/images/logo.png" alt="" class="img-fluid">
+			  </a>
+		  	<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarmain" aria-controls="navbarmain" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="icofont-navigation-menu"></span>
+		  </button>
+	  
+		  <div class="collapse navbar-collapse" id="navbarmain">
+			<ul class="navbar-nav ml-auto">
+			  <li class="nav-item active">
+				<a class="nav-link" href="cetegory.php">Home</a>
+			  </li>
+			  <li class="nav-item active">
+				<a class="nav-link" href="index.php">Login</a>
+			  </li>
+			  <li class="nav-item active">
+				<a class="nav-link" href="register_login.php">Register</a>
+			  </li>
+			  <li class="nav-item active">
+				<a class="nav-link" href="change_password.php">Change Password</a>
+			  </li>
+			</ul>
+		  </div>
+		</div>
+	</nav>
+</header>
