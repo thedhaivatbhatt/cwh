ca93eff4d9a6979d84174c22780830fa81672d74
thedhaivatbhatt <95678661+thedhaivatbhatt@users.noreply.github.com>
2022-05-07 11:30:01 +0530

    project

--- project/admin/cetegory.php
+++ project/admin/cetegory.php
@@ -1,5 +1,6 @@
 <?php
 require_once("inc/header.php");
+require_once("inc/connection.php");
 ?>
 </head>
 
@@ -40,95 +41,95 @@ require_once("inc/header.php");
                                 <h5 class="card-title">Add new category</h5>
                             </div>
                             <div class="card-body">
-                            <form  method="post" enctype="multipart/form-data" action="submit/insert_category.php">
-                                <div class="row g-3">
-                                    <div class="col-md-6">
-                                        <div class="form-floating">
-                                            <input type="email" class="form-control" id='title' name='title' placeholder="Category title" required>
-                                            <label for="title">Title</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-floating">
-                                            <input type="text" class="form-control" id="description" name='description' placeholder="Description">
-                                            <label for="description">Description</label>
-                                        </div>
-                                    </div>
-
-                                    <div class="col-md-4">
-                                        <fieldset class="row mb-3">
-                                            <legend class="col-form-label col-sm-12 pt-0">Select Advertise type</legend>
-                                            <div class="col-sm-12">
-                                                <div class="form-check">
-                                                    <input required class="form-check-input" type="radio" name="adtype" id="news" value="1" checked>
-                                                    <label class="form-check-label" for="news">
-                                                        News Paper
-                                                    </label>
-                                                </div>
-                                                <div class="form-check">
-                                                    <input required class="form-check-input" type="radio" name="adtype" id="website" value="2">
-                                                    <label class="form-check-label" for="website">
-                                                        Website/Application
-                                                    </label>
-                                                </div>
-
+                                <form method="post" enctype="multipart/form-data" action="submit/insert_category.php">
+                                    <div class="row g-3">
+                                        <div class="col-md-6">
+                                            <div class="form-floating">
+                                                <input type="text" class="form-control" id='title' name='title' placeholder="Category title" required>
+                                                <label for="title">Title</label>
                                             </div>
-                                        </fieldset>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <fieldset class="row mb-3">
-                                            <legend class="col-form-label col-sm-12 pt-0">Select type of Advertisement</legend>
-                                            <div class="col-sm-12">
-                                                <div class="form-check">
-                                                    <input required class="form-check-input" type="radio" name="subadtype" id="standard" value="1" checked>
-                                                    <label class="form-check-label" for="standard">
-                                                        Stanard (Black & White)
-                                                    </label>
-                                                </div>
-                                                <div class="form-check">
-                                                    <input required class="form-check-input" type="radio" name="subadtype" id="Classified" value="2">
-                                                    <label class="form-check-label" for="Classified">
-                                                        Classified (multicolor)
-                                                    </label>
-                                                </div>
-                                                <div class="form-check">
-                                                    <input required class="form-check-input" type="radio" name="subadtype" id="Animation" value="3">
-                                                    <label class="form-check-label" for="Animation">
-                                                        Animation
-                                                    </label>
-                                                </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-floating">
+                                                <input type="text" class="form-control" id="description" name='description' placeholder="Description">
+                                                <label for="description">Description</label>
                                             </div>
-                                        </fieldset>
-                                    </div>
+                                        </div>
 
-                                    <div class="col-md-4">
-                                        <div class="form-floating">
-                                            <input type="number" class="form-control" id="pageno" name="pageno" placeholder="Page No">
-                                            <label for="floatingZip">Page No</label>
+                                        <div class="col-md-4">
+                                            <fieldset class="row mb-3">
+                                                <legend class="col-form-label col-sm-12 pt-0">Select Advertise type</legend>
+                                                <div class="col-sm-12">
+                                                    <div class="form-check">
+                                                        <input required class="form-check-input" type="radio" name="adtype" id="news" value="1" checked>
+                                                        <label class="form-check-label" for="news">
+                                                            News Paper
+                                                        </label>
+                                                    </div>
+                                                    <div class="form-check">
+                                                        <input required class="form-check-input" type="radio" name="adtype" id="website" value="2">
+                                                        <label class="form-check-label" for="website">
+                                                            Website/Application
+                                                        </label>
+                                                    </div>
+
+                                                </div>
+                                            </fieldset>
                                         </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="form-floating">
-                                            <input type="text" class="form-control" id="adsize" name='adsize' placeholder="Ad Size">
-                                            <label for="adsize">Ad Size</label>
+                                        <div class="col-md-4">
+                                            <fieldset class="row mb-3">
+                                                <legend class="col-form-label col-sm-12 pt-0">Select type of Advertisement</legend>
+                                                <div class="col-sm-12">
+                                                    <div class="form-check">
+                                                        <input required class="form-check-input" type="radio" name="subadtype" id="standard" value="1" checked>
+                                                        <label class="form-check-label" for="standard">
+                                                            Stanard (Black & White)
+                                                        </label>
+                                                    </div>
+                                                    <div class="form-check">
+                                                        <input required class="form-check-input" type="radio" name="subadtype" id="Classified" value="2">
+                                                        <label class="form-check-label" for="Classified">
+                                                            Classified (multicolor)
+                                                        </label>
+                                                    </div>
+                                                    <div class="form-check">
+                                                        <input required class="form-check-input" type="radio" name="subadtype" id="Animation" value="3">
+                                                        <label class="form-check-label" for="Animation">
+                                                            Animation
+                                                        </label>
+                                                    </div>
+                                                </div>
+                                            </fieldset>
                                         </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="form-floating">
-                                            <input type="number" class="form-control" id="rate" name='rate' name='rate' placeholder="Rate">
-                                            <label for="rate">Rate</label>
+
+                                        <div class="col-md-4">
+                                            <div class="form-floating">
+                                                <input type="number" class="form-control" id="pageno" name="pageno" placeholder="Page No">
+                                                <label for="floatingZip">Page No</label>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <label for="sample">Select sample file</label>
-                                        <div class="form-floating">
-                                            <input type="file" class="form-control-file" id="sample" name='sample' placeholder="Select Sample">
+                                        <div class="col-md-4">
+                                            <div class="form-floating">
+                                                <input type="text" class="form-control" id="adsize" name='adsize' placeholder="Ad Size">
+                                                <label for="adsize">Ad Size</label>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="text-end">
-                                        <button type="submit" class="btn btn-primary">Save</button>
-                                        <button type="reset" class="btn btn-secondary">Reset</button>
-                                    </div>
+                                        <div class="col-md-4">
+                                            <div class="form-floating">
+                                                <input type="number" class="form-control" id="rate" name='rate' name='rate' placeholder="Rate">
+                                                <label for="rate">Rate</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label for="sample">Select sample file</label>
+                                            <div class="form-floating">
+                                                <input type="file" class="form-control-file" id="sample" name='sample' placeholder="Select Sample">
+                                            </div>
+                                        </div>
+                                        <div class="text-end">
+                                            <button type="submit" class="btn btn-primary">Save</button>
+                                            <button type="reset" class="btn btn-secondary">Reset</button>
+                                        </div>
                                     </div>
                                 </form><!-- End floating Labels Form -->
                             </div>
@@ -137,7 +138,7 @@ require_once("inc/header.php");
                             <div class="row">
                                 <div class="col-12">
                                     <h5 class="card-titel border-bottom">Existing Categories</h5>
-                                    <table class="table datatale">
+                                    <table id="table_id" class="table datatable dataTable-table">
                                         <thead>
                                             <th scope="col">#</th>
                                             <th scope="col">title</th>
@@ -149,22 +150,50 @@ require_once("inc/header.php");
                                             <th scope="col">size</th>
                                         </thead>
                                         <tbody>
-                                            <tr>
-                                                <th scope="row">1</th>
-                                                <td>first ad <br>
-                                                    <a href='#'><i class="bi bi-trash-fill"></i></a>
-                                                    <a href='#'><i class="bi bi-pencil-square"></i></a>
-                                                </td>
-                                                <td>
-                                                    <img src="https://picsum.photos/100" class="img-fluid">
-                                                </td>
-                                                <td>200</td>
-                                                <td>News Paper</td>
-                                                <td>Black White</td>
-                                                <td>10</td>
-                                                <td>10x12</td>
-                                            </tr>
+                                            <?php
+                                            $sql = "SELECT * FROM category ORDER BY id DESC";
+                                            $statement = $db->prepare($sql);
+                                            $statement->execute();
+                                            $count = 1;
+                                            while ($row = $statement->fetch()) {
+                                                extract($row);
+                                                // var_dump($row);
 
+                                            ?>
+                                                <tr>
+                                                    <td scope="row"><?php echo $count++; ?></td>
+                                                    <td><?php echo $title; ?> <br>
+                                                        <a href='#'><i class="bi bi-trash-fill"></i></a>
+                                                        <a href='#'><i class="bi bi-pencil-square"></i></a>
+                                                    </td>
+                                                    <td>
+                                                        <img src="/images/category/<?php echo $sampleimage ?>" width="80px">
+                                                    </td>
+                                                    <td><?php echo $rate ?></td>
+                                                    <td>
+                                                        <?php
+                                                        if ($adtype == 1) {
+                                                            echo "News Paper";
+                                                        } else {
+                                                            echo "Website/Application";
+                                                        }
+                                                        ?>
+                                                    </td>
+                                                    <td>
+                                                        <?php
+                                                        if ($subadtype == 1) {
+                                                            echo "Stanard (Black & White) ";
+                                                        } elseif ($subadtype == 2) {
+                                                            echo "Classified (multicolor)";
+                                                        } else {
+                                                            echo "Animation";
+                                                        }
+                                                        ?>
+                                                    </td>
+                                                    <td><?php echo $pageno ?></td>
+                                                    <td><?php echo $adsize ?></td>
+                                                </tr>
+                                            <?php } ?>
                                         </tbody>
                                     </table>
                                 </div>
