c2bd739d6eaa98673785ee4649c30360239bee5e
thedhaivatbhatt <95678661+thedhaivatbhatt@users.noreply.github.com>
2022-06-13 19:51:12 +0530

    jquery

---
+++ php/name.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+function name()
+{
+    print 'Good Morning....'.'<br>';
+    print 'What is your Roll No.?'.'<br>';
+    $roll_no = 12;
+    print 'My Roll No. is : '.$roll_no.'<br>';
+    print 4567.67;
+}
+name();
+?>
+</body>
+</html>
